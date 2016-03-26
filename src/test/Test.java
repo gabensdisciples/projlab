c7382613da9a07a20a90071c1cd1eb7c0d2f62d4
lapaj <lapaj@lapaj.hu>
2016-03-26 16:52:54 +0100

    Teszteset metódusok

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,72 @@
 package test;
 
-public class Test {
+import main.String;
 
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Menu main");
+
+	}
+	
+	public void walkFloorOrWall() {
+		
+	}
+	
+	public void walkGap() {
+		
+	}
+	
+	public void walkSpecWallStarGate() {
+		
+	}
+	
+	public void walkDoor() {
+		
+	}
+	
+	public void walkScale() {
+		
+	}
+	
+	public void pickupZPM() {
+		
+	}
+	
+	public void pickupBox() {
+		
+	}
+	
+	public void pickupForbidden() {
+		
+	}
+	
+	public void placeBox() {
+		
+	}
+	
+	public void placeBoxForbidden() {
+		
+	}
+	
+	public void changeBulletColor() {
+		
+	}
+	
+	public void shootOverWalkable() {
+		
+	}
+	
+	public void shootSpecWall() {
+		
+	}
+	
+	public void shootDoorOrWall() {
+		
+	}
+	
+	public void openHelp() {
+		
+	}
 }
