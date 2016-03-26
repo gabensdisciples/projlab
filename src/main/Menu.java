e5a8c5a7712a16ea4aff576a15da3d71dc1bc850
Norberts11 <norbertss11@gmail.com>
2016-03-26 17:04:46 +0100

    Most jó?

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,18 +1,18 @@
 package main;
 
 public class Menu {
-
+	
 	private menuPoints points;
-
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Menu main");
 
 	}
-
+	
 	public void showHelp() {
 		System.out.println("Menu showHelp");
 	}
-
 }
