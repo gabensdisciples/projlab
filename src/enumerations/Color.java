52a1d2ed0d86d1d84b0fe52686a1d211ad1c2d82
Norberts11 <norbertss11@gmail.com>
2016-04-02 19:13:59 +0200

    Revert "Revert "Revert "player"""
    This reverts commit 550d2f5710d15b211c1df92956faa696befda0fb.

--- src/enumerations/Color.java
+++ src/enumerations/Color.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package enumerations;
 
 import logger.Logger;
@@ -37,43 +36,3 @@ public enum Color {
     return null;
   }
 }
-=======
-package enumerations;
-
-import logger.Logger;
-
-/**
- * Enum class.
- * 
- * @author Gaben's Disciples
- * 
- */
-public enum Color {
-
-  BLUE, YELLOW, GREEN, RED;
-
-  // TODO: Valtozott a szignatura, dokumentalni kell!
-  /**
-   * Gets the other different color form the actual one.
-   * 
-   * @return the color enum value
-   */
-  public Color getOtherColor() {
-    Logger.log("Color getOtherColor");
-    Logger.logout();
-    switch (this) {
-      case BLUE:
-        return Color.YELLOW;
-      case YELLOW:
-        return Color.BLUE;
-      case GREEN:
-        return Color.RED;
-      case RED:
-        return Color.GREEN;
-      default:
-        break;
-    }
-    return null;
-  }
-}
->>>>>>> branch 'master' of https://github.com/gabensdisciples/projlab.git
