df23dafb4663a2ce008228991e3b4a1a40b97f40
Marton Czwick <martonczwick@gmail.com>
2016-04-02 18:59:37 +0200

    player

--- src/cells/Scale.java
+++ src/cells/Scale.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package cells;
 
 import java.util.Stack;
@@ -148,3 +149,155 @@ public class Scale extends LevelObject {
   }
 
 }
+=======
+package cells;
+
+import java.util.Stack;
+
+import enumerations.Direction;
+import enumerations.ItemState;
+import game.Bullet;
+import game.Character;
+import items.Box;
+import items.Item;
+import logger.Logger;
+//változott: default konstruktor, setDoor!
+
+/**
+ * LevelObject class.
+ * 
+ * @author Gaben's Disciples
+ * 
+ */
+public class Scale extends LevelObject {
+  private Stack<Box> box;
+  private Door door;
+  private int limit;
+
+  /**
+   * Default constructor.
+   */
+  public Scale() {
+    super(true);
+  }
+
+  /**
+   * Scale constructor.
+   * 
+   * @param door
+   *          - the door which the scale opens
+   */
+  public Scale(Door door, int limit) {
+    super(true);
+    box = new Stack<Box>();
+    walkable = true;
+    this.door = door;
+    this.limit = limit;
+    Logger.log("Scale konstruktor");
+    Logger.logout();
+  }
+
+  /**
+   * Cell's property.
+   */
+  public ItemState hasItem() {
+    Logger.log("Scale hasItem");
+    Logger.logout();
+    if (!box.isEmpty()) {
+      return ItemState.GOTITEM;
+    } else {
+      return ItemState.NOITEM;
+    }
+  }
+
+  /**
+   * Creates interaction between the character and level objects.
+   */
+  public void interactCharacter(Character character) {
+    Logger.log("Scale interactCharacter");
+    if (box.size() + 1 >= limit) {
+      door.setWalkable(true);
+    }
+    character.setPosition(this);
+    Logger.logout();
+  }
+
+  /**
+   * Creates interaction between the bullet and level objects.
+   */
+  public void interactBullet(Bullet bullet) {
+    Logger.log("Scale interactBullet");
+    bullet.setPosition(this);
+    Logger.logout();
+  }
+
+  @Override
+  public LevelObject getNeighbour(Direction dir, boolean characterCalled) {
+    Logger.log("Scale getNeighbour");
+
+    if (characterCalled && box.size() < limit) {
+      Character dummy = new Character(this, null, dir);
+      LevelObject neighbour = null;
+      switch (dir) {
+        case NORTH:
+          neighbour = neighbourNorth;
+          break;
+        case EAST:
+          neighbour = neighbourEast;
+          break;
+        case SOUTH:
+          neighbour = neighbourSouth;
+          break;
+        case WEST:
+          neighbour = neighbourWest;
+          break;
+        default:
+          break;
+      }
+
+      neighbour.interactCharacter(dummy);
+      if (dummy.getPosition() != this) {
+        door.setWalkable(false);
+      }
+    }
+    Logger.logout();
+    switch (dir) {
+      case NORTH:
+        return neighbourNorth;
+      case EAST:
+        return neighbourEast;
+      case SOUTH:
+        return neighbourSouth;
+      case WEST:
+        return neighbourWest;
+      default:
+        return null;
+    }
+  }
+
+  @Override
+  public void getItem(Character character) {
+    Logger.log("Scale getItem");
+    box.pop().pickUp(character);
+    if (box.size() + 1 < limit) {
+      door.setWalkable(false);
+    }
+    Logger.logout();
+  }
+
+  @Override
+  public void placeItem(Item item) {
+    Logger.log("Scale placeItem");
+    box.push((Box) item);
+    if (box.size() + 1 >= limit) {
+      door.setWalkable(true);
+    }
+    Logger.logout();
+  }
+
+  public void setDoor(Door door) {
+    this.door = door;
+  }
+
+}
+>>>>>>> branch 'master' of https://github.com/gabensdisciples/projlab.git
--- src/main/LevelBuilder.java
+++ src/main/LevelBuilder.java
@@ -13,11 +13,17 @@ import cells.Gap;
 import cells.LevelObject;
 import cells.Scale;
 import cells.SpecWall;
+<<<<<<< HEAD
+import enumerations.Direction;
+import items.Box;
+import items.Zpm;
+=======
 import enumerations.Color;
 import enumerations.Direction;
 import items.Box;
 import items.Zpm;
 import game.Character;
+>>>>>>> branch 'master' of https://github.com/gabensdisciples/projlab.git
 
 /**
  * Singleton utility class, builds a level from txt file.
@@ -33,6 +39,252 @@ public class LevelBuilder {
 
   private static LevelBuilder instance = null;
 
+<<<<<<< HEAD
+  static {
+    objectMap = new HashMap<>();
+    // Empty
+    objectMap.put("E", null);
+    // Walkable floor
+    objectMap.put("FW", new Floor(true, null));
+    // Nonwalkable floor
+    objectMap.put("FNW", new Floor(false, null));
+    // Floor with box
+    objectMap.put("FB", new Floor(true, new Box()));
+    // Floor with zpm
+    objectMap.put("FZ", new Floor(true, new Zpm()));
+    // Door
+    objectMap.put("D", new Door());
+    // Gap
+    objectMap.put("G", new Gap());
+    // Scale (Door = null)
+    objectMap.put("SC", new Scale());
+    // SpecWall
+    objectMap.put("SP", new SpecWall());
+  }
+
+  // Parsed from stringMatrix
+  public LevelObject[][] objectMatrix;
+  // Parsed from txt file
+  public String[][] stringMatrix;
+
+  /**
+   * Creates a single instance of the class.
+   * 
+   * @return the instance
+   */
+  public static LevelBuilder getInstance() {
+    if (instance == null) {
+      instance = new LevelBuilder();
+    }
+    return instance;
+  }
+
+  /**
+   * Constructs the level.
+   * 
+   * @param size
+   *          - size of the square matrix in the text file
+   */
+  public void init(int size) {
+    SIZE = size;
+    stringMatrix = new String[size][size];
+    objectMatrix = new LevelObject[size][size];
+    createStringMatrix();
+    createObjectMatrix();
+    initNeighborDiscovery();
+    setScaleDoor();
+  }
+
+  /**
+   * Creates a square matrix from the text file.
+   */
+  public void createStringMatrix() {
+    try {
+      InputStream stream = ClassLoader.getSystemResourceAsStream("level.txt");
+      BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+      try {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        int row = 0;
+        while ((line = br.readLine()) != null) {
+          String[] values = line.split(" ");
+          for (int column = 0; column < values.length; column++) {
+            stringMatrix[row][column] = values[column];
+          }
+          row++;
+        }
+        String everything = sb.toString();
+        System.out.println(everything);
+      } finally {
+        br.close();
+      }
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    }
+  }
+
+  // Door <--> Scale összerendelés hiányzik
+  /**
+   * Creates a matrix with the appropriate objects from the string one.
+   *
+   * In case of scales and doors it cuts the indentifier from the end of the
+   * string.
+   * 
+   * @return the object matrix
+   */
+  public LevelObject[][] createObjectMatrix() {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        String currentString = stringMatrix[i][j];
+        if (currentString.substring(0, 1).equals("D")) {
+          objectMatrix[i][j] = objectMap.get("D");
+        } else if (currentString.length() >= 2 && currentString.substring(0, 2).equals("SC")) {
+          objectMatrix[i][j] = objectMap.get("SC");
+        } else if (objectMap.get(stringMatrix[i][j]) != null) {
+          objectMatrix[i][j] = objectMap.get(stringMatrix[i][j]);
+        } else {
+          throw new RuntimeException("Wrong abbreviation in text file.");
+        }
+      }
+    }
+    return objectMatrix;
+  }
+
+  /**
+   * Loops through the matrix and sets every object's neighbors.
+   */
+  public void initNeighborDiscovery() {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        setNeighbours(objectMatrix[i][j], i, j);
+      }
+    }
+  }
+
+  /**
+   * Checks the valid neighbors around a level object and sets them.
+   * 
+   * @param levelObject
+   *          - the one who we find neighbors for
+   * @param columnIndex
+   *          - column index
+   * @param rowIndex
+   *          - row index
+   */
+  public void setNeighbours(LevelObject levelObject, int columnIndex, int rowIndex) {
+    // SOUTH
+    if (columnIndex + 1 >= 0 && columnIndex + 1 < SIZE) {
+      levelObject.setNeighbour(Direction.SOUTH, objectMatrix[columnIndex + 1][rowIndex]);
+    }
+    // NORTH
+    if (columnIndex - 1 >= 0 && columnIndex - 1 < SIZE) {
+      levelObject.setNeighbour(Direction.NORTH, objectMatrix[columnIndex - 1][rowIndex]);
+    }
+    // EAST
+    if (rowIndex + 1 >= 0 && rowIndex + 1 < SIZE) {
+      levelObject.setNeighbour(Direction.EAST, objectMatrix[columnIndex][rowIndex + 1]);
+    }
+    // WEST
+    if (rowIndex - 1 >= 0 && rowIndex - 1 < SIZE) {
+      levelObject.setNeighbour(Direction.WEST, objectMatrix[columnIndex][rowIndex - 1]);
+    }
+  }
+
+  /**
+   * Iterates through the matrix, and sets the doors to their scales.
+   */
+  public void setScaleDoor() {
+    Map<String, LevelObject> pairMap = new HashMap<>();
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        String currentString = stringMatrix[i][j];
+
+        // Check if it's a door
+        if (currentString.substring(0, 1).equals("D")) {
+          // If the key is already in the map, we found its scale.
+          if (pairMap.get(currentString) != null) {
+            if ((pairMap.get(currentString) instanceof Scale) == false) {
+              throw new RuntimeException("Unique constraint violation in text file for doors");
+            }
+            Scale scale = (Scale) pairMap.get(currentString);
+            scale.setDoor((Door) objectMatrix[i][j]);
+          } else {
+            pairMap.put(currentString, objectMatrix[i][j]);
+          } // Check fif it's a scale
+        } else if (currentString.length() >= 2 && currentString.substring(0, 2).equals("SC")) {
+          // If the key is already in the map
+          if (pairMap.get(currentString) != null) {
+            if ((pairMap.get(currentString) instanceof Door) == false) {
+              throw new RuntimeException("Unique constraint violation in text file for scales");
+            }
+            Scale scale = (Scale) objectMatrix[i][j];
+            scale.setDoor((Door) pairMap.get(currentString));
+          } else {
+            pairMap.put(currentString, objectMatrix[i][j]);
+          }
+
+        }
+      }
+    }
+  }
+
+  /**
+   * Prints the stringMatrix.
+   */
+  public void printStringMatrix() {
+    for (int i = 0; i < stringMatrix[0].length; i++) {
+      for (int j = 0; j < stringMatrix[0].length; j++) {
+        System.out.print(stringMatrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  /**
+   * Prints the classes of objects in the object matrix.
+   */
+  public void printObjectMatrix() {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        System.out.print(objectMatrix[i][j].getClass() + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  /**
+   * Private constructor for the singleton class.
+   */
+  private LevelBuilder() {
+  }
+
+  public static int getSize() {
+    return SIZE;
+  }
+
+  public static void setSize(int size) {
+    SIZE = size;
+  }
+
+  public LevelObject[][] getObjectMatrix() {
+    return objectMatrix;
+  }
+
+  public void setObjectMatrix(LevelObject[][] objectMatrix) {
+    this.objectMatrix = objectMatrix;
+  }
+
+  public String[][] getStringMatrix() {
+    return stringMatrix;
+  }
+
+  public void setStringMatrix(String[][] stringMatrix) {
+    this.stringMatrix = stringMatrix;
+  }
+
+  public static Map<String, LevelObject> getObjectmap() {
+    return objectMap;
+=======
   private Character oneill;
 
   static {
@@ -290,6 +542,7 @@ public class LevelBuilder {
 
   public void setOneill(Character oneill) {
     this.oneill = oneill;
+>>>>>>> branch 'master' of https://github.com/gabensdisciples/projlab.git
   }
 
 }
