f129c4bc5405ed1f89e3e197cbcf50d9baa77c19
hunakos57 <hunakos57@gmail.com>
2016-05-07 15:17:44 +0200

    Egyszálú megoldás félkész

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,11 +1,19 @@
 package controller;
 
 import model.game.Replicator;
+import model.enumerations.Direction;
 import model.game.Player;
 import model.game.WorkerThread;
 
 public class GameController {
   private WorkerThread worker;
+  
+  
+  //
+  private Player oneill;
+  private Player jaffa;
+  private Replicator replicator;
+  //
   
   public GameController(){
     
@@ -15,12 +23,34 @@ public class GameController {
     synchronized(WorkerThread.instructionQueue){
       WorkerThread.instructionQueue.add(keyCode);
     }
+    switch (WorkerThread.instructionQueue.pollFirst()){
+      case "W": oneill.move(Direction.NORTH);break;
+      case "S": oneill.move(Direction.SOUTH);break;
+      case "A": oneill.move(Direction.WEST);break;
+      case "D": oneill.move(Direction.EAST);break;
+      case "Q": oneill.changeColor();break;
+      case "E": oneill.shoot();break;
+      case "C": oneill.take();break;
+      case "V": oneill.drop();break;
+      case "I": jaffa.move(Direction.NORTH);break;
+      case "K": jaffa.move(Direction.SOUTH);break;
+      case "J": jaffa.move(Direction.WEST);break;
+      case "L": jaffa.move(Direction.EAST);break;
+      case "U": jaffa.changeColor();break;
+      case "O": jaffa.shoot();break;
+      case "N": jaffa.take();break;
+      case "M": jaffa.drop();break;
+      default: break;
+    }
     //worker.notify();
   }
   
   public void startWorkerThread(Player oneill, Player jaffa, Replicator replicator){
-    worker = new WorkerThread(oneill, jaffa, replicator);
-    new Thread(worker).start();
+    this.oneill = oneill;
+    this.jaffa = jaffa;
+    this.replicator = replicator;
+    //worker = new WorkerThread(oneill, jaffa, replicator);
+    //new Thread(worker).start();
   }
 }
 
