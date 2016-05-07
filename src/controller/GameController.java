ea8ce877ab92418e7405be58fdc15f3129daec34
hunakos57 <hunakos57@gmail.com>
2016-05-07 13:48:56 +0200

    WorkerThread és GameController skeleton.

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,7 +1,18 @@
 package controller;
 
+import model.game.Replicator;
+import model.game.Player;
+import model.game.WorkerThread;
+
 public class GameController {
+  private WorkerThread worker;
   
+  public void addKeyPressed(String keyCode){
+    
+  }
+  public void startWorkerThread(Player oneill, Player jaffa, Replicator replicator){
+    
+  }
 }
 
 
--- src/model/game/WorkerThread.java
+++ src/model/game/WorkerThread.java
@@ -1,5 +1,18 @@
 package model.game;
 
-public class WorkerThread {
+import java.util.LinkedList;
 
+public class WorkerThread implements Runnable{
+  private Player oneill;
+  private Player jaffa;
+  private Replicator replicator;
+  public static LinkedList<String> instructionQueue;
+  
+  public WorkerThread(){
+    
+  }
+  
+  public void run(){
+    
+  }
 }
